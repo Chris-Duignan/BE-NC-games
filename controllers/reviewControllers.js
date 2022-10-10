fa967cb0e5fd9399ec72e9971093dcbe6047aeeb
Chris-Duignan <cduignan.1993@gmail.com>
2022-10-10 15:03:25 +0100

    add error handling to get review by id

--- controllers/reviewControllers.js
+++ controllers/reviewControllers.js
@@ -3,7 +3,6 @@ const { selectReviewById } = require("../models/reviewModels");
 exports.getReviewById = (req, res, next) => {
     const {review_id} = req.params;
   selectReviewById(review_id).then((review) => {
-    console.log(review);
     res.status(200).send(review);
   }).catch(next);
 };
--- models/reviewModels.js
+++ models/reviewModels.js
@@ -4,6 +4,10 @@ exports.selectReviewById = (id) => {
   return db
     .query(`SELECT * FROM reviews WHERE review_id = $1;`, [id])
     .then(({ rows: [review] }) => {
-      return review;
+      if (review === undefined) {
+        return Promise.reject({ status: 404, msg: `Id ${id} not Found` });
+      } else {
+        return review;
+      }
     });
 };
