827b0a86b533e2b09e5ccdbb33abf7533ae93404
Chris-Duignan <cduignan.1993@gmail.com>
2022-10-10 12:23:00 +0100

    add happy path test which expects array of category objects

---
+++ __tests__/categories.test.js
@@ -0,0 +1,31 @@
+const app = require("../app");
+const request = require("supertest");
+const db = require("../db/connection");
+const testData = require("../db/data/test-data/index");
+const seed = require("../db/seeds/seed");
+
+beforeEach(() => seed(testData));
+
+afterAll(() => db.end());
+
+describe("GET /api/categories", () => {
+  describe("Happy Path", () => {
+    it("status: 200, responds with array of categories", () => {
+      return request(app)
+        .get("/api/categories")
+        .expect(200)
+        .then(({ body }) => {
+          expect(Array.isArray(body)).toBe(true);
+          expect(body.length).toBe(4);
+          body.forEach((category) => {
+            expect(category).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String),
+              })
+            );
+          });
+        });
+    });
+  });
+});
