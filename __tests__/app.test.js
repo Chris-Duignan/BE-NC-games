4425169a121fa6b443a233e05c79aa3cd07af983
Chris-Duignan <cduignan.1993@gmail.com>
2022-10-12 14:07:11 +0100

    add error handling tests to get reviews queries

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -214,6 +214,22 @@ describe("Review endpoints", () => {
             expect(body.reviews.length).toBe(0);
           });
       });
+      it("status 400: rejects sort_by column not in table", () => {
+        return request(app)
+          .get("/api/reviews?sort_by=bad_request")
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe("Invalid sort query");
+          });
+      });
+      it("status 400: rejects invalid order query", () => {
+        return request(app)
+          .get("/api/reviews?order=left")
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe("Invalid order query");
+          });
+      });
     });
   });
 
@@ -245,14 +261,12 @@ describe("Review endpoints", () => {
         return request(app)
           .get("/api/reviews/3/comments")
           .expect(200)
-          .then(({  body  }) => {
+          .then(({ body }) => {
             expect(body.comments).toBeSortedBy("created_at", {
-              
               descending: true,
-            
             });
-          });;
-      });;
+          });
+      });
     });
     describe("Error Handling", () => {
       it("status 404: return error when review id does not exist", () => {
@@ -298,7 +312,7 @@ describe("Review endpoints", () => {
               author: "dav3rid",
               body: "Totally agree",
               votes: 0,
-              created_at: expect.any(String)
+              created_at: expect.any(String),
             });
           });
       });
@@ -310,46 +324,46 @@ describe("Review endpoints", () => {
           .send({ username: "dav3rid", body: "Totally agree" })
           .expect(404)
           .then(({ body }) => {
-            expect(body.msg).toBe("Id 9999 not Found");;
+            expect(body.msg).toBe("Id 9999 not Found");
           });
       });
       it("status: 400, review id entered in incorrect form", () => {
         return request(app)
           .post("/api/reviews/notAnId/comments")
-          .send({username: "dav3rid", body: "Totally agree"})
+          .send({ username: "dav3rid", body: "Totally agree" })
           .expect(400)
-          .then(({body}) => {
-            expect(body.msg).toBe("Unexpected field type")
-          })
-      })
+          .then(({ body }) => {
+            expect(body.msg).toBe("Unexpected field type");
+          });
+      });
       it("status: 400, request body missing required fields", () => {
         return request(app)
           .post("/api/reviews/1/comments")
           .send({})
           .expect(400)
-          .then(({body}) => {
+          .then(({ body }) => {
             expect(body.msg).toBe("Required fields missing");
-          })
-      })
+          });
+      });
       it("status: 400, responds with error when request fields in incorrect datatype", () => {
         return request(app)
           .post("/api/reviews/1/comments")
-          .send({username: "dav3rid", body: 1234})
+          .send({ username: "dav3rid", body: 1234 })
           .expect(400)
-          .then(({body}) => {
+          .then(({ body }) => {
             expect(body.msg).toBe("Unexpected field type");
-          })
-      })
+          });
+      });
       it("status: 404, responds with error when user doesn't exist", () => {
         return request(app)
           .post("/api/reviews/1/comments")
-          .send({username: "unknown", body: "Hello"})
+          .send({ username: "unknown", body: "Hello" })
           .expect(404)
-          .then(({body}) => {
-            expect(body.msg).toBe("User unknown not found")
-          })
-      })
-    })
+          .then(({ body }) => {
+            expect(body.msg).toBe("User unknown not found");
+          });
+      });
+    });
   });
 
   describe("PATCH /api/reviews/:review_id", () => {
