46d7eadc9097387927e578d84b0cd5d1f889d65a
Chris-Duignan <cduignan.1993@gmail.com>
2022-10-13 09:38:46 +0100

    refactor error handling using rowCount

--- controllers/comment.controllers.js
+++ controllers/comment.controllers.js
@@ -1,11 +1,9 @@
-const { removeCommentById, selectCommentById } = require("../models/comment.models");
+const { removeCommentById } = require("../models/comment.models");
 
 exports.deleteCommentById = (req, res, next) => {
   const { comment_id } = req.params;
 
-  const promises = [selectCommentById(comment_id), removeCommentById(comment_id)]
-
-  Promise.all(promises).then((promises) => {
+  removeCommentById(comment_id).then(() => {
     res.status(204).end();
   }).catch(next)
 };
