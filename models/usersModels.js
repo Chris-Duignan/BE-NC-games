07f94f002352ae8cd730b6ba6d164222616d173a
Chris-Duignan <cduignan.1993@gmail.com>
2022-10-13 15:50:13 +0100

    add error handling functionality to endpoint

--- models/usersModels.js
+++ models/usersModels.js
@@ -1,24 +1,26 @@
 const db = require("../db/connection");
 
 exports.selectUsers = () => {
-    return db
-        .query(
-            `SELECT * FROM users;`
-        ).then(({rows: users}) => {
-            return users;
-        })
+  return db.query(`SELECT * FROM users;`).then(({ rows: users }) => {
+    return users;
+  });
 };
 
-exports.selectUserByUsername = (username) =>{
-    return db
-        .query(
-            `SELECT * from users
-            WHERE username = $1`, [username]
-        ).then(({rows: [user]}) => {
-            if (!user) {
-                return Promise.reject({status: 404, msg: `User ${username} not found`})
-            } else {
-                return user;
-            }
-        })
-}
+exports.selectUserByUsername = (username) => {
+  return db
+    .query(
+      `SELECT * from users
+            WHERE username = $1`,
+      [username]
+    )
+    .then(({ rows: [user] }) => {
+      if (!user) {
+        return Promise.reject({
+          status: 404,
+          msg: `User ${username} not found`,
+        });
+      } else {
+        return user;
+      }
+    });
+};
